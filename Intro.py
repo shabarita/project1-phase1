cc1837e20ca4108868d593957ae69696efa87522
shabarita <147225332+shabarita@users.noreply.github.com>
2023-10-08 22:37:17 -0700

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,1 +1,8 @@
+import pandas as pd # For data analysis
+import matplotlib.pyplot as plt # For plotting
+import seaborn as sns # For better looking plots
+import datetime as dt # For working with dates
 
+# Set some plot style preferences
+sns.set_theme(style="whitegrid")
+plt.rcParams["figure.figsize"] = (10, 6)
